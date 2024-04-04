3706de3dac45ea10eb1d54dd22a7ffb6bbded6cb
princessofnightsky <131848036+princessofnightsky@users.noreply.github.com>
2024-04-04 19:49:21 +0530

    Add files via upload

---
+++ src/Components/Helpers/QuestionBank.js
@@ -0,0 +1,24 @@
+export const Questions =[
+    {
+        prompt: "What is 1+3",
+        optionA: "24",
+        optionB: "4",
+        optionC: "2",
+        answer: "B"
+    },
+    {
+    prompt: "What is the powerhouse of the cell?",
+    optionA: "Mitochondria",
+    optionB: "Nucleus",
+    optionC: "Ribosome",
+    answer: "A"
+    },
+    {
+    prompt: "Which planet is known as the Red Planet?",
+    optionA: "Venus",
+    optionB: "Mars",
+    optionC: "Jupiter",
+    answer: "B"
+    }
+
+];
